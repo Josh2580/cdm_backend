5e84f9f63f06f2436f5461c11a209ebf8a36e0e8
Joshua <joshuaugwuede@gmail.com>
2024-03-18 17:43:31 +0100

    wow

--- coin/models.py
+++ coin/models.py
@@ -4,8 +4,9 @@ from django.utils import timezone
 
 class Mining(models.Model):
     name = models.CharField(default="CARDANO MAZE", max_length=20)
-    quantity_mined = models.PositiveIntegerField(default=0)
+    quantity_mined = models.DecimalField(max_digits=10, decimal_places=2)
     time_clicked = models.DateTimeField()
+    first_click = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
     mineral_extracted = models.CharField(max_length=50, default="")
     extraction_date = models.DateTimeField(default=timezone.now)
